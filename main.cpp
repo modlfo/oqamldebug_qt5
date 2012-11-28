08b498f598c0066214cef36a2dcac26cc402e0ba
Sebastien Fricker <fricker@froglogic.com>
2012-11-28 20:15:03 +0100

    Parsing the command line arguments after calling QApplication

--- main.cpp
+++ main.cpp
@@ -15,15 +15,16 @@ int main( int argc, char *argv[] )
     Q_INIT_RESOURCE( oqamldebug );
 
     Options::read_options();
-    QStringList arguments;
-    for (int i=1; i< argc ; i++)
-        arguments << argv[i];
-
 #ifdef Q_WS_X11
     if (XOpenDisplay(NULL) == 0)
         return 1;
 #endif
     QApplication app( argc, argv );
+
+    QStringList arguments;
+    for (int i=1; i< argc ; i++)
+        arguments << argv[i];
+
     MainWindow mainWin(arguments);
     mainWin.show();
     int ret = app.exec();
