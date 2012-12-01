d8788247258bc282964020dd9993a973c46d9b46
Sebastien Fricker <fricker@froglogic.com>
2012-12-01 13:00:08 +0100

    Factorial sample

--- test.ml
+++ test.ml
@@ -1,16 +1,12 @@
 open Printf
 open Pervasives
 
+let rec fact n = 
+  match n with
+    | 0 -> 1
+    | n -> n * ( fact ( n - 1 ) ) 
+
 let _ = 
-  let s = 0 in
-  let t2 = [ "a" ; "b" ] in
-  let t1 = [ "c" ; "d" ] in
-  let t = [ t1 ; t2 ] in
-    for i = 0 to 10
-    do
-      printf "." ;
-    done ;
-    printf "Size? :" ;
-    let m = read_int () in
-      printf "%i\n" m
+  let n = 5 in
+    printf "fact(%i) = %i\n" n (fact n)
 ;;
