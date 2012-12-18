67042f7a98dcad717274bbfd5825a1b259547d8e
Sébastien Fricker <fricker@froglogic.com>
2012-12-18 11:22:47 +0100

    Calling make install after make

--- test.sh
+++ test.sh
@@ -3,4 +3,5 @@ ocamlc -o testme -g test.ml || exit
 #./testme
 qmake CONFIG+=debug CONFIG-=release CONFIG-=app_bundle
 make -j4 || exit
+make install
 ./bin/oqamldebug testme 'a string with quotes(")'
